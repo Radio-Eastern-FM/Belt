df315df4ed7864e25f348443b99b1783c05a5da8
Mark Blashki <markblashki1@gmail.com>
2022-06-15 16:10:04 +1000

    Rename: 'belt'

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:14
 
-WORKDIR /usr/src/app/scheduler
+WORKDIR /usr/src/app/belt
 
 COPY package*.json ./
 
--- src/services/user-service.js
+++
@@ -1,8 +0,0 @@
-export async function createUser(data) {
-  const response = await fetch(`/api/user`, {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({user: data})
-  })
-  return await response.json();
-}
